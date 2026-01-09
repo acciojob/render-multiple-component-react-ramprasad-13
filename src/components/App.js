0fbbe0bc97cf33019637fb7b9c047db7e91f1182
ram prasad <ramprasad@MacBookAir.lan>
2026-01-09 11:18:17 +0530

    assessment try1

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,24 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  const [projects, setProjects] = useState([
+    {name:'todo',desc:'Todo Application.'},
+    {name:'cancer Prediction',desc:'Cancer prediction using AI/ML.'}
+  ])
+
+  const RenderProjects = projects.map((project)=>{
+    return (
+      <div className="ns-wrapper" >
+        <h1 data-ns-test='project-name' >{project.name}</h1>
+        <h6 data-ns-test='project-description' >{project.desc}</h6>
+      </div>
+    )
+  })
+
   return (
-    <div id="main"></div>
+    <div id="main">
+      {RenderProjects}
+    </div>
   )
 }
 
